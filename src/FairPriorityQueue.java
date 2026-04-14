4cb1e58eb2872542bf4325872badab1202dc197a
David Toghanro <davidtoghanro@wireless-10-148-10-240.public.utexas.edu>
2026-04-13 22:41:33 -0500

    priorityqueue

---
+++ src/FairPriorityQueue.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class FairPriorityQueue<E extends Comparable<E>> {
+    private LinkedList<E> con;
+
+    public FairPriorityQueue() {
+        con = new LinkedList<>();
+    }
+
+    public void enqueue(E val) {
+        int idx = 0;
+        while (idx < con.size() && val.compareTo(con.get(idx)) >= 0) {
+            idx++;
+        }
+        con.add(idx, val);
+    }
+
+    public E dequeue() {
+
+    }
+
+}
